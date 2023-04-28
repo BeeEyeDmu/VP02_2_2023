3355e94a99aa2aec4e8ab905f798ecf1320d2004
BeeEye Dmu <bikang@konyang.ac.kr>
2023-04-28 16:10:20 +0900

    Add files via upload

---
+++ 005_login/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _005_login
+{
+  /// <summary>
+  /// MainWindow.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnLogin_Click(object sender, RoutedEventArgs e)
+    {
+      if (txtId.Text == "abcd" && txtPass.Password == "1234")
+        MessageBox.Show("로그인 성공!");
+      else
+        MessageBox.Show("로그인 실패!");
+    }
+  }
+}
