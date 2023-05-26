6c79793418502bc6e47c6d93670d89376371da0b
BeeEye Dmu <bikang@konyang.ac.kr>
2023-05-26 15:51:47 +0900

    Add files via upload

---
+++ 012_TwoWindows/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _012_TwoWindows
+{
+  /// <summary>
+  /// MainWindow.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnShow_Click(object sender, RoutedEventArgs e)
+    {
+      Window1 w = new Window1();
+      w.Show();
+    }
+
+    private void btnShowDialog_Click(object sender, RoutedEventArgs e)
+    {
+      Window1 w = new Window1();
+      w.ShowDialog();
+    }
+  }
+}
---
+++ 013_Pages/start.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _013_Pages
+{
+  /// <summary>
+  /// start.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class start : Page
+  {
+    public start()
+    {
+      InitializeComponent();
+    }
+
+    private void btnInfo_Click(object sender, RoutedEventArgs e)
+    {
+      NavigationService.Navigate(new Uri("/Information.xaml", UriKind.Relative));
+    }
+
+    private void btnLogin_Click(object sender, RoutedEventArgs e)
+    {
+      NavigationService.Navigate(new Uri("/Login.xaml", UriKind.Relative));
+    }
+
+    private void btnSignUp_Click(object sender, RoutedEventArgs e)
+    {
+      NavigationService.Navigate(new Uri("/SignUp.xaml", UriKind.Relative));
+    }
+  }
+}
