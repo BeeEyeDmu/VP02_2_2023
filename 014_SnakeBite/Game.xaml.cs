9dd4f636266b71265d25442582895e84870a63c6
BeeEye Dmu <bikang@konyang.ac.kr>
2023-06-02 14:14:05 +0900

    Add files via upload

--- 014_SnakeBite/Game.xaml.cs
+++ 014_SnakeBite/Game.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Media;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
@@ -30,7 +24,10 @@ namespace _014_SnakeBite
     private string move = "";   // 움직이는 방향
     private int eaten = 0;      // 먹은 갯수
     DispatcherTimer timer = new DispatcherTimer();
-    Stopwatch sw = new Stopwatch(); 
+    Stopwatch sw = new Stopwatch();
+
+    private bool startFlag = false;
+    SoundPlayer myPlayer;
 
     public Game() // 생성자
     {
@@ -44,7 +41,85 @@ namespace _014_SnakeBite
 
     private void Timer_Tick(object sender, EventArgs e)
     {
-      
+      if (move != "")
+      {
+        startFlag = true;
+        for (int i = visibleCount; i > 0; i--)
+        {
+          Point p = (Point)snakes[i - 1].Tag;
+          snakes[i].Tag = new Point(p.X, p.Y);
+        }
+
+        Point q = (Point)snakes[0].Tag;
+
+        if (move == "U")
+          snakes[0].Tag = new Point(q.X, q.Y - size);
+        else if (move == "D")
+          snakes[0].Tag = new Point(q.X, q.Y + size);
+        else if (move == "R")
+          snakes[0].Tag = new Point(q.X + size, q.Y);
+        else if (move == "L")
+          snakes[0].Tag = new Point(q.X - size, q.Y);
+
+        EatEgg();     // 알을 먹었는지 체크
+      }
+
+      if (startFlag == true)
+      {
+        TimeSpan ts = sw.Elapsed;
+        txtTimer.Text =
+          String.Format("Time = {0:00}:{1:00}.{2:00}",
+          ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+        DrawSnakes(); // 뱀을 그려주는 메소드
+      }
+    }
+
+    private void EatEgg()
+    {
+      Point pS = (Point)snakes[0].Tag;
+      Point pE = (Point)egg.Tag;
+
+      if(pS.X == pE.X && pS.Y == pE.Y)
+      {
+        egg.Visibility = Visibility.Hidden;
+        visibleCount++;
+        snakes[visibleCount-1].Visibility = Visibility.Visible; // 꼬리 하나 늘림
+        txtScore.Text = "Eggs = " + ++eaten;
+
+        if(visibleCount == 30)  // 게임종료
+        {
+          timer.Stop();
+          sw.Stop();
+          DrawSnakes();
+          TimeSpan ts = sw.Elapsed;
+          string time =
+            String.Format("Time = {0:00}:{1:00}.{2:00}",
+            ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+          MessageBox.Show("Success!!! " + time + " sec");
+          this.Close();
+        }
+        else
+        {
+          // 안보이게 만들었던 알을 위치를 새로 지정하고 보이게 한다
+          int x = r.Next(1, (int)(field.Width) / size - 1);
+          int y = r.Next(1, (int)(field.Height) / size - 1);
+          Point p = new Point(x * size, y * size);
+          egg.Tag = p;
+          egg.Visibility = Visibility.Visible;
+          Canvas.SetLeft(egg, p.X);
+          Canvas.SetTop(egg, p.Y);
+        }
+      }
+    }
+
+    private void DrawSnakes()
+    {
+      for(int i=0; i<visibleCount; i++)
+      {
+        Point p = (Point)snakes[i].Tag;
+        Canvas.SetLeft(snakes[i], p.X);
+        Canvas.SetTop(snakes[i], p.Y);
+      }
     }
 
     private void InitSnakes()
@@ -88,6 +163,27 @@ namespace _014_SnakeBite
       field.Children.Add(egg);
       Canvas.SetLeft(egg, x * size);
       Canvas.SetTop(egg, y * size);
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+      timer.Start();
+      sw.Start();
+
+      if (e.Key == Key.Left)
+        move = "L";
+      else if (e.Key == Key.Right)
+        move = "R";
+      else if (e.Key == Key.Up)
+        move = "U";
+      else if (e.Key == Key.Down)
+        move = "D";
+      else if (e.Key == Key.Escape)
+      {
+        move = "";
+        timer.Stop();
+        sw.Stop();
+      }
     }
   }
 }
